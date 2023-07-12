bb260f26ae26d35ccb480f2b30211af8527ef737
Mavis021 <077BCT043.mamata@pcampus.edu.np>
2023-07-12 22:47:25 +0545

    scalable window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+
 #define windowx 800
-#define windowy 800
+#define windowy 600
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
 	glfwInit();
@@ -11,7 +20,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(windowx, windowy, "Window yayyy!", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(windowx, windowy, "SpaceEdition", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -22,22 +31,44 @@ int main()
 
 	// Introducing the window into the current context
 	glfwMakeContextCurrent(window);
-	gladLoadGL();
+	
+	//initialize GLAD before we call any OpenGL function because GLAD manages function pointers for OpenGL
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << " Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
 	glViewport(0, 0, windowx, windowy);
-	// Specifying the color of the background
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	// Clean the back buffer and assign the new color to it
-	glClear(GL_COLOR_BUFFER_BIT);
-	// Swap the back buffer with the front buffer
-	glfwSwapBuffers(window);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//the render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		//keyboard input
+		processInput(window);
+
+		//rendering commands
+	
+			// Specifying the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
 		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
+
 	glfwTerminate();
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
